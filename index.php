1cf06833fa1263c986f28fce23c5161cd61b4da8
Ute Klotz <ute.klotz@gapps.hswlu.ch>
2013-10-27 14:22:22 +0100

    erstens

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>Ute's Bootstrap</title>
+</head>
+<body>
+<header>
+<h1>My First Test</h1>
+</header>
+<article>
+<p>
+Mein erster Versuch in der Cloud
+</p>
+</article>
+<?php
+echo "ok?";
+?>
+<p>
+Ende des Versuchs
+</p>
+</body>
+</html>
